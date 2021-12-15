0fc3c289e6badae95e23e04bc6bd30b9c89f288d
M. Fadli Zein <45892408+gzeinnumer@users.noreply.github.com>
2021-12-15 12:53:34 +0700

    Initial commit

---
+++ app/src/main/java/com/gzeinnumer/countintoverpower/MainActivity.java
@@ -0,0 +1,60 @@
+package com.gzeinnumer.countintoverpower;
+
+import static com.gzeinnumer.countintoverpower.helper.GblFunction.idr;
+import static com.gzeinnumer.countintoverpower.helper.GblFunction.idrComma;
+import static com.gzeinnumer.countintoverpower.helper.GblFunction.saparator;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    private List<String> productBuys = new ArrayList<>();
+    TextView tv1;
+    TextView tv2;
+    TextView tv3;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        tv1 = findViewById(R.id.tv1);
+        tv2 = findViewById(R.id.tv2);
+        tv3 = findViewById(R.id.tv3);
+
+        productBuys.add("9999");
+        productBuys.add("9999");
+        productBuys.add("9999");
+        productBuys.add("9999");
+        productBuys.add("9999");
+        productBuys.add("9999");
+        productBuys.add("9999");
+        productBuys.add("9999");
+        productBuys.add("9999");
+
+        hitungTotal();
+    }
+
+    public void hitungTotal() {
+        double sum = 0;
+        for (int j = 0; j < productBuys.size(); j++) {
+            if (productBuys.get(j) != null) {
+
+                double harga1 = Double.parseDouble("9999999");
+                double qty1 = Double.parseDouble(productBuys.get(j));
+
+                Double subtotal1 = (harga1 * qty1);
+                sum = Math.round(subtotal1 + sum);
+            }
+        }
+        tv1.setText(saparator(String.valueOf(sum)));
+        tv2.setText(idrComma(String.valueOf(sum)));
+        tv3.setText(idr(String.valueOf(sum)));
+    }
+}
